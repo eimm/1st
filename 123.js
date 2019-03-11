d1fa6ac852a2f40602d63003507c923ebba18a16
max <mopo34ek@gmail.com>
2019-03-11 22:58:39 +0300

    1st

---
+++ 123.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log("Norm");
+// let ans = prompt ('Ты гей', 'y');
+// console.log(ans);
+let incr = 10,
+    decr = 10;
+decr--;
+incr++;
+
+console.log(incr);
+console.log(decr);
+console.log(5%2);
+console.log(2 === '2');
+let ch = false,
+    chs = false;
+console.log(ch || !chs);
