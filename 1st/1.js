5db9224b5571b7d21339c6bf1594e2b7e5d10faf
max <mopo34ek@gmail.com>
2019-03-12 00:36:43 +0300

    5th

--- 1st/1.js
+++ 1st/1.js
@@ -1,18 +1,40 @@
 'use strict'
-let money = prompt ('Ваш бюджет на месяц', "");
+let money = +prompt ('Ваш бюджет на месяц', "");
 let time = prompt ('Введите дату в формате YYYY-MM-DD','YYYY-MM-DD');
-let fir = prompt ('Введите обязательную статью расходов в этом месяце');
-let sec = prompt ('Во сколько обойдется?');
-var appData = {
+// let a1 = prompt ('Введите обязательную статью расходов в этом месяце');
+// let a2 = prompt ('Во сколько обойдется?');
+// let b1 = prompt ('Введите обязательную статью расходов в этом месяце');
+// let b2 = prompt ('Во сколько обойдется?');
+
+let appData = {
     budget : money,
     timeData : time,
-    expenses : {
-        first : sec 
-    },
-    optionalExpenses : {
-    },
+    expenses : {},
+    optionalExpenses : {},
     income : [],
     savings : false
 } ;
-let res = (appData.budget-appData.expenses.first)/30;
-alert (res); 
+for (let i =0 ; i <2; i++){
+    let a = prompt ('Введите обязательную статью расходов в этом месяце');
+    let b = prompt ('Во сколько обойдется?');
+    if ((typeof(a) === 'string') && (typeof(a) != null)&& (typeof(b) != null)&&(a!='')&&(b!='')&&(a.length<50)) {
+        console.log('ex');
+        appData.expenses[a]=b;
+    }else {
+        alert ('Вводи нормально, а?');
+        i--;
+
+
+    }
+    
+}
+appData.moneyPerDay = appData.budget/30;
+
+alert ('Ежедневный бюджет' + appData.moneyPerDay); 
+if (appData.moneyPerDay < 100){
+    console.log('bomzh');
+}else if (appData.moneyPerDay < 1000){
+    console.log('norm');
+}else {
+    console.log('vahse norm');
+}
