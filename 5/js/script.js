bf153ee657709b6224a6050dd326a3169bc8e4c7
max <mopo34ek@gmail.com>
2019-03-13 18:05:19 +0300

    13.03 5dz

--- 5/js/script.js
+++ 5/js/script.js
@@ -16,17 +16,26 @@ let web = {
         console.log(newB);
         console.log(btn);
         let btnTemp1 = btn[1],
-            btnTemp2 = btn [2];
+            btnTemp2 = btn[2];
         buttons.insertBefore(btnTemp2,btnTemp1);
-        
-        
-            
-
-        
-
-        
+    },
+    changeTitle : function () {
+        let title = document.querySelector('.title');
+        title.innerHTML="Мы продаем только подлинную технику Apple";
+    },
+    ask : function () {
+        let ans = prompt('Как вы относитесь к технике Apple?', ""),
+            anns = document.querySelector('.prompt');
+        anns.innerHTML=ans;
+    },
+    changeImg : function (){
+        document.body.style.backgroundImage = 'url(/Users/user/Documents/ws/5/img/apple_true.jpg)';  
     }
 };
-// web.deleteAdv();
+web.deleteAdv();
 web.addButton();
+web.changeTitle();
+web.ask();
+web.changeImg();
+
 
