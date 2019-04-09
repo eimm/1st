f1f88698a07003e37a8b20d50d52dc22db4511a6
max <mopo34ek@gmail.com>
2019-04-09 19:16:20 +0300

    09.04

--- 7/7.js
+++ 7/7.js
@@ -33,6 +33,40 @@ window.addEventListener('DOMContentLoaded',function(){
         }
 
     });
+    let deadline = '2019-05-05';
+
+    function getTimeRemaining (endtime){
+        let t = Date.parse(endtime)-Date.parse(new Date()),
+            seconds = Math.floor((t/1000)%60),
+            minutes = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/(1000*60*60)));
+        return {
+            'total':t,
+            'hours':hours,
+            'minutes':minutes,
+            'seconds':seconds
+        };
+    }
+
+    function setClock (id,endtime){
+        let timer = document.getElementById(id),
+            hours=timer.querySelector('.hours'),
+            minutes=timer.querySelector('.minutes'),
+            seconds=timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock,1000);
+        function updateClock(){
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours.toString().padStart(2,0);
+            minutes.textContent = t.minutes.toString().padStart(2,0);;
+            seconds.textContent = t.seconds.toString().padStart(2,0);;
+
+            if (t.total<= 0){
+                clearInterval(timeInterval);
+            }
+        }
+            
+    }
+    setClock('timer',deadline);
 });
 
  
